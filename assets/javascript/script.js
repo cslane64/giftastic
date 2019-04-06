b81edc151928e2a7b7397d62074aa8933b669d0b
cslane64 <cslane@gmail.com>
2019-04-06 14:18:15 -0400

    two thirds of the way done

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -29,14 +29,50 @@ function showButtons() {
  function showFoodGifs() {
 
     var foodCat = $(this).attr("data-name");
-    console.log(foodCat);
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + foodCat + "&api_key=cVsb8aLGA8HZ0xBrjmxV8Fl7EhKOewfB&limit=10";
-
+    
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
-      $("#image-farm").text(response.rating);
+      var results = response.data
+      $("#image-farm").empty();
+      for (i=0; i < results.length; i++) {
+      
+      //console.log(response);
+      // create div to hold the fooditems
+      var  foodDiv= $("<div class='foods'>");
+      // store rating data
+      var rating = results[i].rating;
+      var foodRating = $("<h4>").text("rating: " + rating);
+      console.log(foodRating);
+      
+      //display the rating data
+      foodDiv.append(foodRating);
+    
+
+      // store image url
+      var imageUrl = results[i].images.fixed_height.url;
+      // create an element to display the URL
+      var foodImage = $("<img>").attr("src", imageUrl);
+      console.log(foodImage);
+      //display the url
+      foodDiv.append(foodImage);
+      //appending the movie 
+
+      $("#image-farm").append(foodDiv);
+    }   
+
+
+      
+      
+      
+      
+      
+
+      //$("#image-farm").append(foodImage);
+      //$("#image-farm").append(foodRating);
+
     });
   }
   $(document).on("click", ".foodItem", showFoodGifs);
